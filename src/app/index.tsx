28512f58bdc4b4619e2bb2675c7fb5dee6447967
Ramom Oliveira <ra-mom2000@hotmail.com>
2024-07-11 16:05:10 -0300

    style configuration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-zinc-400">Hello World!</Text>
     </View>
   );
 }
